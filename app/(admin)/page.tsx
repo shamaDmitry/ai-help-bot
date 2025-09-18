a086cfe185550480c2ad361f4a1a72fe7cc20d5f
shama <shama.dmitry@gmail.com>
2025-09-18 18:02:08 +0300

    feat: restructure Home component layout with Card and improve code readability

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,22 +1,36 @@
 import Title from "@/components/Title";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <pre>
-        {JSON.stringify(
-          process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
-          null,
-          2
-        )}
-      </pre>
+    <main className="w-full">
+      <Card className="max-w-2xl mx-auto">
+        <CardContent className="flex flex-col items-center justify-center">
+          <pre>
+            {JSON.stringify(
+              process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+              null,
+              2
+            )}
+          </pre>
 
-      <pre>{JSON.stringify(process.env.VERCEL_ENV, null, 2)}</pre>
+          <pre>{JSON.stringify(process.env.VERCEL_ENV, null, 2)}</pre>
 
-      <Title>
-        Welcome to AI Help Bot! Build and deploy AI chatbots for your website in
-        minutes.
-      </Title>
+          <Title>
+            Welcome to AI Help Bot! Build and deploy AI chatbots for your
+            website in minutes.
+          </Title>
+
+          <Link href="/create-chatbot" className="">
+            <Button>
+              Create bot <CirclePlus />
+            </Button>
+          </Link>
+        </CardContent>
+      </Card>
     </main>
   );
 }
